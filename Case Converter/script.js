10ca9da8d9a959926beba9a015f9e89ff4bf9f77
Karan Vishwakarma <karanvishwakarma3007@gmail.com>
2026-05-03 17:46:10 +0530

    Add files via upload
    This is a small project that is to solve my sownproblem of case conversion while doing a few tasks of m owny. So to solve that problem O have nuilded a case coverter for myself.

---
+++ Case Converter/script.js
@@ -0,0 +1,26 @@
+function getText() {
+    return document.getElementById("tInput").value;
+}
+
+function setText(value) {
+    document.getElementById("tInput").value = value;
+}
+
+function toUpper() {
+    let text = getText();
+    setText(text.toUpperCase());
+}
+
+function toLower() {
+    let text = getText();
+    setText(text.toLowerCase());
+}
+
+function toTitle() {
+    let text = getText();
+    let result = text
+        .toLowerCase()
+        .replace(/\b\w/g, letter => letter.toUpperCase());
+
+    setText(result);
+}
